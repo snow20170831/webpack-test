99a985ed19b2dcb9198c41cf7941aa61cab35d9c
xuejiao.zheng <xuejiao.zheng@xuncetech.com>
2019-07-03 17:22:56 +0800

    Code Splitting的异步代码分割

--- src/index.js
+++ src/index.js
@@ -87,12 +87,28 @@
  * 当业务逻辑发生变化时，只要加载main.js即可 (1mb)
  * */
 
-// 同步加载
-import _ from 'lodash';
+// // 同步加载
+// import _ from 'lodash';
 
-// 业务逻辑1mb
-console.log(_.join(['a', 'b', 'c'], '***'));
-console.log(_.join(['a', 'b', 'd'], '***'));
+// // 业务逻辑1mb
+// console.log(_.join(['a', 'b', 'c'], '***'));
+// console.log(_.join(['a', 'b', 'd'], '***'));
 
 
- 
+ // 异步加载
+ function getComponent() {
+     return import('lodash').then(() => {
+         var element = document.createElement('div');
+         element.innerHTML = _.join(['code', 'splitting'], '-');
+         return element;
+     })
+ }
+
+ getComponent().then(element => {
+     document.body.appendChild(element);
+ })
+
+ // 代码分割，和webpack无关
+ // webpack中实现代码分割，两种方式
+ // 1. 同步代码：只需要在webpack.common.js中做optimization的配置即可
+ // 2. 异步代码（import）：异步代码，无需做任何配置，会自动进行代码分割，放置到新的文件中
